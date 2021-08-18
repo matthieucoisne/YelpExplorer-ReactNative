2632efefd669843ea2006bd7f3113d60565dcb91
Matthieu Coisne <matthieu.coisne@gmail.com>
2021-08-18 19:57:31 -0400

    Added BusinessListQuery

---
+++ src/data/graphql/BusinessListQuery.ts
@@ -0,0 +1,29 @@
+import { gql } from "@apollo/client";
+
+export const businessListQuery = gql`
+  query BusinessList(
+    $term: String
+    $location: String!
+    $sortBy: String
+    $limit: Int
+  ) {
+    search(term: $term, location: $location, sort_by: $sortBy, limit: $limit) {
+      total
+      business {
+        id
+        name
+        photos
+        rating
+        review_count
+        location {
+          address1
+          city
+        }
+        price
+        categories {
+          title
+        }
+      }
+    }
+  }
+`;
