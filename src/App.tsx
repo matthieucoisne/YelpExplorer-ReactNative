de8afb8da53d19e84e825084dd8141d224ab95ad
Matthieu Coisne <matthieu.coisne@gmail.com>
2021-08-18 19:54:54 -0400

    moved source files to a "src" folder moved AppRegistry into index.ts

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import { ApolloProvider } from "@apollo/client";
+import { apolloClient } from "./data/graphql/client";
+import BusinessListScreen from "./features/business/BusinessListScreen";
+
+const App = () => (
+  <ApolloProvider client={apolloClient}>
+    <BusinessListScreen />
+  </ApolloProvider>
+);
+
+export default App;
