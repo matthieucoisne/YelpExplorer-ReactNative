873de33a478ba7f129cb269798524550ef75aefa
Matthieu Coisne <matthieu.coisne@gmail.com>
2021-08-30 22:36:13 -0400

    removed expo

--- index.ts
+++
@@ -1,7 +0,0 @@
-import { AppRegistry } from "react-native";
-import { expo } from "./app.json";
-import App from "./src/App";
-
-AppRegistry.registerComponent(expo.name, () => App);
-
-export default App;
