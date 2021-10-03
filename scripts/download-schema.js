8b6d9cbd33015c2bc9a13948b60bac6bec082adf
Matthieu Coisne <matthieu.coisne@gmail.com>
2021-10-03 14:22:27 -0400

    updated scripts

--- scripts/download-schema.js
+++ scripts/download-schema.js
@@ -1,10 +1,13 @@
+// This script depends on `apollo-cli`.
+// > brew install apollo-cli
+
 const exec = require('child_process').exec;
 const appConfig = require('../config/app_config.json');
 
 const command = `apollo client:download-schema \
                     --header=\"Authorization: Bearer ${appConfig.api_key}\" \
                     --endpoint=https://api.yelp.com/v3/graphql \
-                    ./src/data/graphql/schema/schema.json`;
+                    ./src/features/business/data/graphql/schema/schema.json`;
 
 exec(command, function (error, stdout, stderr) {
   if (error !== null) {
